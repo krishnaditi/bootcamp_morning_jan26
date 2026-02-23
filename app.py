a95ee30de4ebd47a66e2b372e7007c624398e1f8
krishnaditi <krishanaaditi@gmail.com>
2026-02-23 10:49:16 +0530

    updated the admin_dashboard

--- app.py
+++ app.py
@@ -36,12 +36,38 @@ def login():
         email = request.form['email']
         user = User.query.filter_by(username=username).first()
         if user:
-            return redirect(url_for('user_dashboard', username=user.username))
+            if username == 'admin':
+                return redirect(url_for('admin_dashboard'))
+            elif user.status == 'accepted':
+                return redirect(url_for('user_dashboard', username=user.username))
+            else:
+                return "Your account is not approved yet."
     return render_template('login.html')
 
 @app.route('/user_dashboard')
 def user_dashboard():
     return render_template('user_dashboard.html')
+
+@app.route('/admin_dashboard')
+def admin_dashboard():
+    user = User.query.all()
+    return render_template('admin_dashboard.html', user=user)
+
+@app.route('/approve_user/<int:user_id>', methods=['POST'])
+def approve_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        user.status = 'accepted'
+        db.session.commit()
+    return redirect(url_for('admin_dashboard'))
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query']
+        results = User.query.filter(User.username.contains(query)).all()
+        return render_template('admin_dashboard.html', user=results)
+    return render_template('admin_dashboard.html', user=User.query.all())
 
 with app.app_context():
     db.create_all()
